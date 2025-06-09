c74222c1b11abb7902c1a7b52a25ce3b82471e8a
pikoseq9 <wekpat@wp.pl>
2025-06-09 16:27:41 +0200

    init

---
+++ SchoolSystem/Model/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Model
+{
+    internal class Enums
+    {
+        //np. typy użytkownika, ocen itp
+
+    }
+}
---
+++ SchoolSystem/View/Dashboards/PrincipalDashboardView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolSystem.View.Dashboards
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PrincipalDashboardView.xaml
+    /// </summary>
+    public partial class PrincipalDashboardView : UserControl
+    {
+        public PrincipalDashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
