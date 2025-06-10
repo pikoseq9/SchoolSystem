c1da297623b22980fc4b83ba708f5ff900b0a0c2
pikoseq9 <wekpat@wp.pl>
2025-06-10 16:54:09 +0200

    db based model

--- SchoolSystem/Model/Grade.cs
+++ SchoolSystem/Model/Grade.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace SchoolSystem.Model
 {
-    internal class Grade
+    public class Grade : Enums
     {
+        public int ID { get; set; }
+        public int StudentID { get; set; }
+        public int SubjectID { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+        public GradeType Category { get; set; }
+        public int Weight { get; set; }
+
+        public Grade(int iD, int studentID, int subjectID, DateTime date, decimal value, GradeType category, int weight)
+        {
+            ID = iD;
+            StudentID = studentID;
+            SubjectID = subjectID;
+            Date = date;
+            Value = value;
+            Category = category;
+            Weight = weight;
+        }
     }
 }
--- SchoolSystem/Model/Student.cs
+++ SchoolSystem/Model/Student.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace SchoolSystem.Model
 {
-    internal class Student
+    public class Student : Enums
     {
+        public int Id { get; set; }
+        public int ClassID { get; set; }
+        public string? Name { get; set; }
+        public string? SurName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Sex? Gender { get; set; }
+        public string? PESEL { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public Student(int id, int classID, string? name, string? surName, DateTime? dateOfBirth, Sex? gender, string? pESEL, string? login, string? password)
+        {
+            Id = id;
+            ClassID = classID;
+            Name = name;
+            SurName = surName;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            PESEL = pESEL;
+            Login = login;
+            Password = password;
+        }
     }
 }
