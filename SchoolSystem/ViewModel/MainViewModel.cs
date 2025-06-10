8794f24066a0e718a8e7f34a29313972191b8d34
Kacper Śliwka <kacpersliwka2003@gmail.com>
2025-06-10 19:31:38 +0200

    Logowanie

--- SchoolSystem/ViewModel/MainViewModel.cs
+++ SchoolSystem/ViewModel/MainViewModel.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SchoolSystem.ViewModel
 {
-    internal class MainViewModel
+    using BaseClass;
+    using SchoolSystem.View;
+    using SchoolSystem.View.Dashboards;
+
+    class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ShowLoginPageCommand { get; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentView)));
+            }
+        }
+
+        public MainViewModel()
+        {
+            ShowLoginPageCommand = new RelayCommand(o => ShowLoginPage(), null);
+        }
+
+        public void ShowUserMenuPage()
+        {
+            CurrentView = new UserMenu();
+        }
+
+        private void ShowLoginPage()
+        {
+            var loginView = new LoginView();
+
+            if (loginView.DataContext is LoginViewModel loginVM)
+            {
+                loginVM.OnLoginSuccess = ShowUserMenuPage;  // podłącz akcję do przejścia na UserMenu
+            }
+
+            CurrentView = loginView;
+        }
     }
+
+
 }
