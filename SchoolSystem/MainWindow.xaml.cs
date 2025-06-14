063bfabfad4ccfae33a9776672bfcb8fedb4e748
pikoseq9 <wekpat@wp.pl>
2025-06-14 22:42:20 +0200

    add remark page + add teacher full name to remark model

--- SchoolSystem/MainWindow.xaml.cs
+++ SchoolSystem/MainWindow.xaml.cs
@@ -28,27 +28,6 @@ namespace SchoolSystem
         public MainWindow()
         {
             InitializeComponent();
-
-            TestDatabaseConnectionAndDataLoad();
-        }
-
-        private void TestDatabaseConnectionAndDataLoad()
-        {
-            StudentRepository studentrepository = new StudentRepository();
-            Graderepository graderepository = new Graderepository();
-            Remarkrepository remarkrepository = new Remarkrepository();
-            ObservableCollection<Student>? students = new ObservableCollection<Student>();
-            ObservableCollection<Grade>? grades = new ObservableCollection<Grade>();
-            ObservableCollection<Remark>? remarks = new ObservableCollection<Remark>();
-
-            grades = graderepository.GetAllGrades();
-            students = studentrepository.GetAllStudentsByClassId(1);
-            remarks = remarkrepository.GetAllRemarksByStudentId(1);
-            MessageBox.Show(students[0].Name + students[0].SurName + students[0].DateOfBirth);
-            
-            //MessageBox.Show($"{grades.Count}");
-            //MessageBox.Show($"{students.Count}");
-
         }
     }
 }
--- SchoolSystem/View/Pages/RemarkPage.xaml.cs
+++ SchoolSystem/View/Pages/RemarkPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SchoolSystem.ViewModel;
 
 namespace SchoolSystem.View.Pages
 {
@@ -23,6 +24,7 @@ namespace SchoolSystem.View.Pages
         public RemarkPage()
         {
             InitializeComponent();
+            this.DataContext = new RemarkViewModel();
         }
     }
 }
